3aececa1e0280476de4cbee1bf7b342949ffa2d5
Dale Buckley <dalebuckley86@gmail.com>
2013-09-19 15:24:46 +0100

    Moved over advertiser assistant to main view controller

--- RopeGameController/SPMainViewController.h
+++ RopeGameController/SPMainViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+@import MultipeerConnectivity;
 
-@interface SPMainViewController : UIViewController
+@interface SPMainViewController : UIViewController <MCSessionDelegate>
 
 @end
