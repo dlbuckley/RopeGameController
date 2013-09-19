9a88eabeecc33ac48693bf7f28dafc92811f54d0
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2013-09-19 11:10:59 +0200

    added players view controller

---
+++ SPPlayersViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPPlayersViewController.h
+//  RopeGameController
+//
+//  Created by Valerio Mazzeo on 19/09/13.
+//  Copyright (c) 2013 NSSpain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPPlayersViewController : UITableViewController
+
+@end
